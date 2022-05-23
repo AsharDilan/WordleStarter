e9a27c869af1d33fad2b04fd9c6af5148bd19146
AsharDilan <ashardi@ssfs.org>
2022-05-22 20:55:20 -0400

    Hi Brian, This code has a problem that I don't know how to resolve. The problem is that I don't think it is accessing the Dictionary because it always says word not found. If there is anything you know how to resolve this, then it would be great if you could let me know. Thanks, Dilan

--- Wordle.java
+++ Wordle.java
@@ -5,28 +5,64 @@
  */
 
 public class Wordle {
+    private int randomIndex = (int) (Math.random() * WordleDictionary.FIVE_LETTER_WORDS.length);
+    private String answerToWordle = WordleDictionary.FIVE_LETTER_WORDS[randomIndex].toUpperCase();
 
     public void run() {
         gw = new WordleGWindow();
         gw.addEnterListener((s) -> enterAction(s));
     }
 
-/*
- * Called when the user hits the RETURN key or clicks the ENTER button,
- * passing in the string of characters on the current row.
- */
-
     public void enterAction(String s) {
-        gw.showMessage("You have to implement this method.");
+        String userWord = s;
+        userWord = userWord.toLowerCase();
+        boolean check = false;
+        for (String checkWord : WordleDictionary.FIVE_LETTER_WORDS) {
+            if (userWord.equals(checkWord)) {
+                check = true;
+            }
+        }
+        if (!check) {
+            gw.showMessage("not in word list.");
+
+        } else {
+            gw.showMessage("in word list.");
+        }
+        for (int col = 0; col < WordleGWindow.N_COLS; col++) {
+            if (userWord.substring(col, col + 1).equals(answerToWordle))
+            {
+                gw.setSquareColor(gw.getCurrentRow(), col, WordleGWindow.CORRECT_COLOR);
+            }
+            if (gw.getSquareLetter(gw.getCurrentRow(), col).compareTo(answerToWordle) != -1) {
+                gw.setSquareColor(gw.getCurrentRow(), col, WordleGWindow.PRESENT_COLOR);
+            } else {
+                gw.setSquareColor(gw.getCurrentRow(), col, WordleGWindow.MISSING_COLOR);
+            }
+
+        }
+        updateRow();
     }
 
-/* Startup code */
 
     public static void main(String[] args) {
         new Wordle().run();
     }
-
-/* Private instance variables */
+    public void RandomWord()
+    {
+        int letter = 0;
+        for(int c = 0; c < WordleGWindow.N_COLS; c++)
+        {
+            gw.setSquareLetter(0,c,answerToWordle.substring(letter,letter+1));
+            letter++;
+        }
+    }
+    public void updateRow(){
+        int rowIndex = 1;
+        if(rowIndex <= WordleGWindow.N_ROWS)
+        {
+            gw.setCurrentRow(rowIndex);
+        }
+    }
 
     private WordleGWindow gw;
 
